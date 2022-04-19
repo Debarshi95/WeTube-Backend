5b24034856c423a11d7499f709013c9fa6dbfac8
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-04-19 21:46:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const connectToDB = require('./utils/dbConnect');
+
+const path = '/graphql';
+const port = 4000;
+
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: (context) => context,
+});
+
+connectToDB();
+server.start().then(() => {
+  server.applyMiddleware({ path, app });
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
