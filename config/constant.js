1b50db8a3e6004c521d59f0bc665877cbd43693f
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-04-27 22:04:28 +0530

    Added Prisma setup for data modelling

---
+++ config/constant.js
@@ -0,0 +1,5 @@
+module.exports = {
+  JWT_SECRET: process.env.JWT_SECRET,
+  PORT: process.env.PORT,
+  PATH: process.env.PATH,
+};
---
+++ seeds.js
@@ -0,0 +1,194 @@
+[
+  {
+    name: 'Technology',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'Science',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'Trending',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'Movies',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    name: 'Sports',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+/////////////////////
+
+[
+  {
+    CategoryId: 1,
+    VideoId: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 1,
+    VideoId: 3,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 1,
+    VideoId: 4,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 1,
+    VideoId: 5,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 1,
+    VideoId: 6,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 1,
+    VideoId: 7,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 1,
+    VideoId: 17,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 2,
+    VideoId: 16,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 2,
+    VideoId: 18,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 2,
+    VideoId: 13,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 2,
+    VideoId: 14,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 2,
+    VideoId: 15,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 2,
+    VideoId: 17,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 3,
+    VideoId: 7,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 3,
+    VideoId: 9,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 3,
+    VideoId: 19,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 3,
+    VideoId: 20,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 3,
+    VideoId: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 3,
+    VideoId: 6,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 4,
+    VideoId: 19,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 4,
+    VideoId: 20,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 5,
+    VideoId: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 5,
+    VideoId: 8,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 5,
+    VideoId: 9,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 5,
+    VideoId: 10,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 5,
+    VideoId: 11,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    CategoryId: 5,
+    VideoId: 12,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
